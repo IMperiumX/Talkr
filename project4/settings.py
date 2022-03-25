5f269112b7a41d43caa79b634985efed8e7720ef
Restyled.io <commits@restyled.io>
2022-03-25 14:33:51 +0000

    Restyled by isort

--- project4/settings.py
+++ project4/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-from django.urls import reverse_lazy
 import os
+
+from django.urls import reverse_lazy
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
