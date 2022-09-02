78d3a3ad90d1062457b4f573bdfa6c94bd80121b
yusufadell <youssef.31057404@ics.tanta.edu.eg>
2022-09-02 21:34:00 +0200

    feat(CBVs): user class based views instead of views

--- network/urls.py
+++ network/urls.py
@@ -4,18 +4,21 @@ from . import views
 
 urlpatterns = [
     # AUTH URLs
-    path("", views.PostListView.as_view(), name="index"),
     # Post related URLs
-    path('like/', views.like, name='like'),
-    path('post/new', views.create_post, name='post_new'),
-    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
-    path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_edit'),
-    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
+    path("", views.post_list_view, name="index"),
+    path("like/", views.like, name="like"),
+    path("post/new", views.create_post, name="post_new"),
+    path("post/<int:pk>/", views.post_detail_view, name="post_detail"),
+    path("post/<int:pk>/edit/", views.post_update_view, name="post_edit"),
+    path("post/<int:pk>/delete/", views.post_delete_view, name="post_delete"),
     # User action realted URLs
-    path('edit/', views.edit, name='edit'),
-    path('users/follow/', views.user_follow, name='user_follow'),
-    path('users/', views.UserPostListView.as_view(), name='user_list'),
-    path('users/<username>/following/',
-         views.following_posts, name='user_following'),
-    path('users/<username>/', views.user_detail, name='user_detail'),
+    path("edit/", views.post_update_view, name="edit"),
+    path("users/follow/", views.user_follow, name="user_follow"),
+    path("users/", views.user_list_view, name="user_list"),
+    path("users/<username>/following/", views.following_posts, name="user_following"),
+    path(
+        "users/<username>/",
+        views.user_detail_view,
+        name="user_detail",
+    ),
 ]
--- network/views.py
+++ network/views.py
@@ -1,21 +1,41 @@
 from itertools import chain
 
-from django.contrib import messages
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.models import User
-# Follow system packages.
-from django.http import Http404, HttpResponseRedirect, JsonResponse
-from django.shortcuts import get_object_or_404, render
-from django.urls import reverse, reverse_lazy
-from django.views.decorators.csrf import csrf_exempt
-from django.views.decorators.http import require_POST
-from django.views.generic import DeleteView, DetailView, ListView, UpdateView
-
 from actions.utils import create_action
 from common.decorators import ajax_required
-from .forms import CreatePostForm, ProfileEditForm, UserEditForm
+from django.contrib import messages
+from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
+from django.http import HttpResponseRedirect, JsonResponse
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse, reverse_lazy
+from django.utils.translation import gettext_lazy as _
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+from django.views.generic import (
+    CreateView,
+    DeleteView,
+    DetailView,
+    ListView,
+    UpdateView,
+)
+
+from .forms import CreatePostForm
 from .models import Contact, Post
+
+User = get_user_model()
+
+
+# TODO: refactor (CBV & optimize iterable flatting logic)
+@login_required
+def following_posts(request, username):
+    user = User.objects.get(username=username, is_active=True)
+    user_to_set = user.following.all()
+    following_post = [user.twitter_posts.all() for user in user_to_set]
+    posts = list(chain.from_iterable(following_post))
+
+    return render(request, "network/following.html", {"posts": posts})
 
 
 @login_required
@@ -33,72 +53,69 @@ def create_post(request):
     return render(request, "network/post_new.html", {"form": CreatePostForm()})
 
 
-# TODO: Use CBV instead
-def user_detail(request, username):
-    user = get_object_or_404(User, username=username, is_active=True)
-    posts = user.twitter_posts.all()
-    return render(request, "user/detail.html", {"user": user, "posts": posts})
+class UserDetailView(DetailView):
+    model = User
+    template_name = "user/detail.html"
+    slug_url_kwarg = "username"
+    slug_field = "username"
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(is_active=True)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["posts"] = self.object.twitter_posts.published()
+        return context
 
 
-# TODO: refactor (CBV & optimize iterable flatting logic)
-@login_required
-def following_posts(request, username):
-    user = User.objects.get(username=username, is_active=True)
-    user_to_set = user.following.all()
-    following_post = [user.twitter_posts.all() for user in user_to_set]
-    posts = list(chain.from_iterable(following_post))
-
-    return render(request, "network/following.html", {"posts": posts})
+user_detail_view = UserDetailView.as_view()
 
 
-@login_required
-def edit(request):
-    if request.method == "POST":
-        user_form = UserEditForm(instance=request.user, data=request.POST)
-        profile_form = ProfileEditForm(
-            instance=request.user.profile, data=request.POST, files=request.FILES
-        )
-        if user_form.is_valid() and profile_form.is_valid():
-            user_form.save()
-            profile_form.save()
-            messages.success(request, "Profile updated successfully")
-        else:
-            messages.error(request, "Error updating your profile")
-    else:
-        user_form = UserEditForm(instance=request.user)
-        profile_form = ProfileEditForm(instance=request.user.profile)
-    return render(
-        request,
-        "network/edit.html",
-        {"user_form": user_form, "profile_form": profile_form},
-    )
+class UserUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
+
+    model = User
+    fields = ["date_of_birth", "photo", "username", "email"]
+    success_message = _("Information successfully updated")
+
+    def get_success_url(self):
+        assert (
+            self.request.user.is_authenticated
+        )  # for mypy to know that the user is authenticated
+        return self.request.user.get_absolute_url()
+
+    def get_object(self):
+        return self.request.user
+
+
+user_update_view = UserUpdateView.as_view()
 
 
 @login_required
 @csrf_exempt
 def like(request):
     if request.method == "POST":
-        post_id = request.POST.get('id')
-        is_liked = request.POST.get('is_liked')
+        post_id = request.POST.get("id")
+        is_liked = request.POST.get("is_liked")
         try:
             post = Post.objects.get(id=post_id)
-            if is_liked == 'no':
+            if is_liked == "no":
                 post.users_like.add(request.user)
-                is_liked = 'yes'
-            elif is_liked == 'yes':
+                is_liked = "yes"
+            elif is_liked == "yes":
                 post.users_like.remove(request.user)
-                is_liked = 'no'
+                is_liked = "no"
             post.save()
 
             return JsonResponse(
                 {
-                    'like_count': post.users_like.count(),
-                    'is_liked': is_liked,
+                    "like_count": post.users_like.count(),
+                    "is_liked": is_liked,
                     "status": 201,
                 }
             )
         except:
-            return JsonResponse({'error': "Post not found", "status": 404})
+            return JsonResponse({"error": "Post not found", "status": 404})
     return JsonResponse({}, status=400)
 
 
@@ -106,29 +123,30 @@ def like(request):
 @require_POST
 @login_required
 def user_follow(request):
-    user_id = request.POST.get('id')
-    action = request.POST.get('action')
+    user_id = request.POST.get("id")
+    action = request.POST.get("action")
     if user_id and action:
         try:
             user = User.objects.get(id=user_id)
-            if action == 'follow':
-                Contact.objects.get_or_create(
-                    user_from=request.user, user_to=user)
-                create_action(request.user, 'is following', user)
+            if action == "follow":
+                Contact.objects.get_or_create(user_from=request.user, user_to=user)
+                create_action(request.user, "is following", user)
             else:
-                Contact.objects.filter(
-                    user_from=request.user, user_to=user).delete()
-            return JsonResponse({'status': 'ok'})
+                Contact.objects.filter(user_from=request.user, user_to=user).delete()
+            return JsonResponse({"status": "ok"})
         except User.DoesNotExist:
-            return JsonResponse({'status': 'error'})
-    return JsonResponse({'status': 'error'})
+            return JsonResponse({"status": "error"})
+    return JsonResponse({"status": "error"})
 
 
 class PostListView(ListView):
-    queryset = Post.published.all()
+    queryset = Post.objects.published()
     context_object_name = "posts"
     paginate_by = 10
     template_name = "network/index.html"
+
+
+post_list_view = PostListView.as_view()
 
 
 class PostDetailView(DetailView):
@@ -136,33 +154,32 @@ class PostDetailView(DetailView):
     template_name = "network/post_detail.html"
 
 
+post_detail_view = PostDetailView.as_view()
+
+
 class PostUpdateView(LoginRequiredMixin, UpdateView):
     model = Post
     template_name = "network/post_edit.html"
     fields = ["body", "status"]
-    success_url = reverse_lazy("index")
-
-    def dispatch(self, request, *args, **kwargs):
-        obj = self.get_object()
-        if obj.author != self.request.user:
-            raise Http404("You are not allowed to edit this Post")
-        return super(PostUpdateView, self).dispatch(request, *args, **kwargs)
 
 
-class PostDeleteView(DeleteView):
+post_update_view = PostUpdateView.as_view()
+
+
+class PostDeleteView(LoginRequiredMixin, DeleteView):
     model = Post
     template_name = "network/post_delete.html"
     success_url = reverse_lazy("index")
 
-    def dispatch(self, request, *args, **kwargs):
-        obj = self.get_object()
-        if obj.author != self.request.user:
-            raise Http404("You are not allowed to delete this Post")
-        return super(PostUpdateView, self).dispatch(request, *args, **kwargs)
+
+post_delete_view = PostDeleteView.as_view()
 
 
-class UserPostListView(ListView):
+class UserListView(ListView):
     queryset = User.objects.filter(is_active=True).exclude(id=1)
     paginate_by = 10
     context_object_name = "users"
     template_name = "user/list.html"
+
+
+user_list_view = UserListView.as_view()
