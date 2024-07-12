04fe9aa7904399d8dc06b7e7e5ff9ac8afd4d1e6
IMperiumX <yusufadell.dev@gmail.com>
2024-07-12 19:51:04 +0300

    refactor: Update post types and reactions in network constants

---
+++ network/constants.py
@@ -0,0 +1,15 @@
+# Post types
+TEXT = "post"
+VIDEO = "video"
+
+# Post Status
+DRAFT = "draft"
+PUBLISHED = "published"
+
+# Post Reactions
+LIKE = "Like"
+LOVE = "Love"
+LAUGHT = "Laugh"
+SAD = "Sad"
+CARE = "Care"
+ANGRY = "Angry"
